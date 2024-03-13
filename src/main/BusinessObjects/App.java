3a1c0d6819974f8d2a869db1260b1b821c4b3637
idkteh <124367770+idkteh@users.noreply.github.com>
2024-03-13 16:36:18 +0000

    Added dominik's code

--- src/main/BusinessObjects/App.java
+++ src/main/BusinessObjects/App.java
@@ -27,8 +27,17 @@ public class App {
             } else if (code == 0) {
                 System.out.println("\nCar already exists in table");
                 }
+
             System.out.println("\nCall deleteById() where id = 14");
             IUserDao.deleteCarById(14);
+
+            System.out.println("\n Call: findCarById()");
+            CarClass car = IUserDao.findCarById(2);
+            if(car != null){ //null is returned if in is not valid
+                System.out.println("Car found: " + car);
+            } else {
+                System.out.println("Car with this id not found in database");
+            }
 
         } catch (DaoException e) {
             e.printStackTrace();
@@ -39,7 +48,7 @@ public class App {
 
 /* TODO ask dermot to help with the output cause why is there '
     Features 1 - Dominik - done
-    Feature 2 - Dominik
+    Feature 2 - Dominik - done
     Feature 3 - Ida - done
     Feature 4 - Logan - done
  */
--- src/main/DAOs/CarDaoInterface.java
+++ src/main/DAOs/CarDaoInterface.java
@@ -9,4 +9,5 @@ public interface CarDaoInterface {
     public List<CarClass> findAllCars() throws DaoException;
     public void deleteCarById(int id) throws DaoException;
     public int  insertCar(CarClass car) throws DaoException;
+    public CarClass findCarById(int id) throws DaoException;
 }
