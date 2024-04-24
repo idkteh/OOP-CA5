846b5a939f298d027c7101bd9a42fd20f8df9093
idkteh <124367770+idkteh@users.noreply.github.com>
2024-04-24 13:13:49 +0100

    Everyting up to date

--- src/main/java/BusinessObjects/ClientServerCommands.java
+++
@@ -1,9 +0,0 @@
-package main.java.BusinessObjects;
-
-public class ClientServerCommands {
-    public String DisplayCarById = "display car by id";
-    public String DisplayAllCars = "display all cars";
-    public String DeleteCarById = "delete car by id";
-    public ClientServerCommands() {
-    }
-}
